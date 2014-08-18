132bab8f5c9bcb4939d5f49979623519c9d47a55
chan <chanwangv@gmail.com>
2014-08-18 17:37:14 +0800

    maybe wrong

--- B1007.c
+++ B1007.c
@@ -1,12 +1,35 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
 	int i,j;
+	int k=1;
+	int flag;
+	int d,count=0;
 	int n;
 	scanf("%d",&n);
-	for(i=0;i<=n;i++)
+	int a[n/2+1];
+	a[0]=2;
+	for(i=3;i<=n;i++)
 	{
-		if()
+		flag=0;
+		for(j=2;j<(sqrt(i)+1);j++)
+		{	if(i%j==0)
+			{	flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		{	a[k]=i;
+			k++;
+		}
 	}
+	for(i=1;i<n/2+1;i++)
+	{
+		d=a[i]-a[i-1];
+		if(d==2)
+			count++;
+	}
+	printf("%d",count);
 	return 0;
 }
