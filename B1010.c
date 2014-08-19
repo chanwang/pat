f17c4f33783cb65953d2f2616656c19a1195221a
chan <chanwangv@gmail.com>
2014-08-19 14:10:44 +0800

    maybe use gets or ch=getchar then change it to integer

--- B1010.c
+++ B1010.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 int main()
 {
-	int i,j;
+	int i,j,n,x,max;
 	int a[1001]={0},b[1001]={0};
+	scanf("%d%d",&x,&n);
+	a[n]=x;
+	max=n;
+	while(n!='\n'&&x!='\n')
+	{
+		scanf("%d%d",&x,&n);
+		a[n]=x;
+	}
+	for(i=max;i>=0;i--)
+	{
+		if(a[i]!=0)
+		printf("%d %d ",a[i],i);
+	}
 	return 0;
 }
