6ecc40e8ba59d4481e27a1b10462ac92b96aef01
chan <chanwangv@gmail.com>
2014-08-29 10:36:21 +0800

    Series-to:1067651156@qq.com Series-cc:chanwangv@gmail.com

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double n;
+	double s;
+	scanf("%lf",&n);
+	s=sqrt(n);
+	printf("%lf",s+1);
+//	printf("int %d\n",sizeof(int));
+/*	int i=0,len;
+	char str[40]="happy";
+	for(i=0;str[i]!='\0';i++)
+		;
+	printf("%d\n",i);
+	for(i=0;str[i]!='\0';i++)
+		putchar(str[i]);
+*/
+	return 0;
+}
